8de8727c0731048b238ba94b5aa377ffb9996be2
kaustav.sarkar <kaustavsarkar.1925@gmail.com>
2023-07-05 04:04:31 +0530

    Adding a demo code for using chromedp

--- main/main.go
+++ main/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/chromedp/chromedp"
+)
+
+const url = "https://main.sci.gov.in/judgments"
+
+func main() {
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	var data string
+
+	if err := chromedp.Run(ctx, chromedp.Navigate(url),
+		chromedp.OuterHTML("html", &data, chromedp.ByQuery)); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(data)
+
+}
