88284332343f5d5815407777834ea212622e8cc2
kaustav.sarkar <kaustavsarkar.1925@gmail.com>
2023-07-08 04:15:02 +0530

    Adding a method to traverse the output directory

--- filereader/reader.go
+++ filereader/reader.go
@@ -2,6 +2,7 @@ package filereader
 
 import (
 	"io/ioutil"
+	"os"
 )
 
 func GetHtml(filePath string) (string, error) {
@@ -12,4 +13,20 @@ func GetHtml(filePath string) (string, error) {
 
 	// Convert the file content to a string
 	return string(fileContent), nil
+}
+
+func TraverseOutputDir(root string) ([]string, error) {
+	dirEntries, err := os.ReadDir(root)
+	if err != nil {
+		return nil, err
+	}
+
+	dirNames := make([]string, 0)
+
+	for _, entry := range dirEntries {
+		if entry.IsDir() {
+			dirNames = append(dirNames, entry.Name())
+		}
+	}
+	return dirNames, nil
 }
