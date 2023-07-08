ba0ea411c0083c9d5a4e4c99627047652200fed4
kaustav.sarkar <kaustavsarkar.1925@gmail.com>
2023-07-08 20:57:33 +0530

    Adding code to interact with the judgement data and save it in sqlite

---
+++ db/db.go
@@ -0,0 +1,90 @@
+package db
+
+import (
+	"database/sql"
+	"errors"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Judgement struct {
+	DiaryNumber        string
+	CaseNumber         string
+	PetitionerName     string
+	RespondentName     string
+	PetitionerAdvocate string
+	RespondentAdvocate string
+	Bench              string
+	JudgementBy        string
+	JudgementLinks     string
+}
+
+func Open() (*sql.DB, error) {
+	db, dbErr := sql.Open("sqlite3", "judgements.db")
+	if dbErr != nil {
+		return nil, dbErr
+	}
+
+	createTableSql := `
+	CREATE TABLE IF NOT EXISTS judgements (
+		DiaryNumber TEXT,
+		CaseNumber TEXT,
+		PetitionerName TEXT,
+		RespondentName TEXT,
+		PetitionerAdvocate TEXT,
+		RespondentAdvocate TEXT,
+		Bench TEXT,
+		JudgementBy TEXT,
+		JudgementLinks TEXT,
+
+
+	)
+	`
+	if _, execErr := db.Exec(createTableSql); execErr != nil {
+		return nil, execErr
+	}
+
+	return db, nil
+}
+
+func Close(db *sql.DB) error {
+	return db.Close()
+}
+
+func (j *Judgement) Insert(db *sql.DB) error {
+	stmt, prepErr := db.Prepare("INSERT INTO judgements(DiaryNumber, CaseNumber, PetitionerName, RespondentName, PetitionerAdvocate, RespondentAdvocate, Bench, JudgementBy, JudgementLinks)")
+	if prepErr != nil {
+		return prepErr
+	}
+	defer stmt.Close()
+
+	_, execErr := stmt.Exec(j.DiaryNumber, j.CaseNumber, j.PetitionerName,
+		j.RespondentName, j.PetitionerAdvocate, j.RespondentAdvocate, j.Bench,
+		j.JudgementBy, j.JudgementLinks)
+	if execErr != nil {
+		return prepErr
+	}
+	return nil
+}
+
+func (j *Judgement) ReadAll(db *sql.DB) ([]*Judgement, error) {
+	selectQuery := "SELECT * FROM judgements"
+	rows, qErr := db.Query(selectQuery)
+	if qErr != nil {
+		return nil, qErr
+	}
+	defer rows.Close()
+
+	var judgements []*Judgement
+	var readErr error
+
+	for rows.Next() {
+		var judgement Judgement
+		err := rows.Scan(&judgement.DiaryNumber, &judgement.CaseNumber, &judgement.PetitionerName, &judgement.RespondentName, &judgement.PetitionerAdvocate, &judgement.RespondentAdvocate, &judgement.Bench, &judgement.JudgementBy, &judgement.JudgementLinks)
+
+		readErr = errors.Join(readErr, err)
+		judgements = append(judgements, &judgement)
+
+	}
+	return judgements, readErr
+}
